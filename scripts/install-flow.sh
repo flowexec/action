5ead029b303438219c7647ea0c6715c164d6baad
Jahvon Dockery <jahvondockery@gmail.com>
2025-07-04 13:57:33 -0400

    fix versioned download

--- scripts/install-flow.sh
+++ scripts/install-flow.sh
@@ -7,7 +7,16 @@ if [ "${FLOW_VERSION:-latest}" = "latest" ]; then
     curl -sSL https://raw.githubusercontent.com/jahvon/flow/main/scripts/install.sh | bash
 else
     echo "Installing flow version: $FLOW_VERSION"
-    curl -sSL "https://github.com/jahvon/flow/releases/download/$FLOW_VERSION/flow_linux_amd64.tar.gz" | tar -xz
+
+    OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+    ARCH=$(uname -m)
+    case $ARCH in
+        x86_64) ARCH="amd64" ;;
+        arm64|aarch64) ARCH="arm64" ;;
+        *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+    esac
+
+    curl -sSL "https://github.com/jahvon/flow/releases/download/$FLOW_VERSION/flow_${OS}_${ARCH}.tar.gz" | tar -xz
     sudo mv flow /usr/local/bin/flow
 fi
 
